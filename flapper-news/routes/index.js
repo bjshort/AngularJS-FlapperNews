caa7892a92bc600fe4f61242173c2ab687c2a1ab
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2015-04-15 21:05:56 +0100

    Authenticaition

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,145 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt = require('express-jwt');
+
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET All posts. */
+router.get('/posts', function(req, res, next){
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+    res.json(posts);
+  });
+});
+
+/* Adding a new post */
+router.post('/posts', auth, function(req, res, next){
+  var post = new Post(req.body);
+  post.author = req.payload.username;
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+/* Binds id parameter to fetching a post */
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if(!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+/* Get a post by id */
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if(err) { return next(err); }
+    res.json(req.post);
+  });
+});
+
+/* Increasing the upvote of a Post */
+router.put('/posts/:post/upvote', auth, function(req, res, next){
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+/* Adding a comment with a post attached */
+router.post('/posts/:post/comments', auth, function(req, res, next){
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  comment.author = req.payload.username;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post){
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+/* Binds id parameter to fetching a comment */
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if(!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+/* Increasing the upvote of a Comment */
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next){
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+/* --- AUTH ---- */
+
+/* Register */
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields.'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+  user.setPassword(req.body.password);
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()});
+  });
+});
+
+/* Login */
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields.'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  });
 });
 
 module.exports = router;
